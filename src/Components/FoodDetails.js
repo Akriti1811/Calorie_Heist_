9abf29437400638187cf5e0b591406c0502c22f1
Akriti Singh <akritisingh1811@gmail.com>
2021-12-15 00:08:27 +0530

    classname changed

--- src/Components/FoodDetails.js
+++ src/Components/FoodDetails.js
@@ -2,39 +2,39 @@ import React from 'react'
 
 export default function FoodDetails(props) {
     return (
-        <div class="col-md-7">
-                <div class="card-body">
-                    <h5 class="card-title px-3">{props.name}</h5>
-                    <p class="card-text px-3">Quantiy</p>
+        <div className="col-md-7">
+                <div className="card-body">
+                    <h5 className="card-title px-3">{props.name}</h5>
+                    <p className="card-text px-3">Quantiy</p>
 
-                    <div class="detail-card px-2 py-2">
-                        <div class="d-flex flex-wrap">                
-                            <div class="col-md-4 col-12 px-2">
+                    <div className="detail-card px-2 py-2">
+                        <div className="d-flex flex-wrap">                
+                            <div className="col-md-4 col-12 px-2">
                                <h6>Calories</h6>
                                <p>{props.cal}</p>
                             </div>  
 
-                            <div class="col-md-4 col-12 px-2">
+                            <div className="col-md-4 col-12 px-2">
                                <h6>Carbohydrates</h6>
                                <p>{props.carbs}</p>
                             </div>
 
-                            <div class="col-md-4 col-12 px-2">
+                            <div className="col-md-4 col-12 px-2">
                                <h6>Cholesterol</h6>
                                <p>{props.colestrol}</p>
                             </div>
 
-                            <div class="col-md-4 col-12 px-2">
+                            <div className="col-md-4 col-12 px-2">
                                <h6>Sugar</h6>
                                <p>{props.sugar}</p>
                             </div>
 
-                            <div class="col-md-4 col-12 px-2">
+                            <div className="col-md-4 col-12 px-2">
                                <h6>Fat</h6>
                                <p>{props.fat}</p>
                             </div>
                   
-                            <div class="col-md-4 col-12 px-2">
+                            <div className="col-md-4 col-12 px-2">
                                <h6>Protein</h6>
                                <p>{props.protein}</p>
                             </div>
--- src/Components/Login.js
+++ src/Components/Login.js
@@ -3,24 +3,24 @@ import React from 'react'
 export default function Login() {
   
     return (
-    <div class="mt-3 pt-3">
-        <div class=" py-5" >
-            <h2 class="text-center py-4">LOGIN</h2>
-            <div class="col-md-4 col-12 form-inner pb-5 px-5 mx-auto">
+    <div className="mt-3 pt-3">
+        <div className=" py-5" >
+            <h2 className="text-center py-4">LOGIN</h2>
+            <div className="col-md-4 col-12 form-inner pb-5 px-5 mx-auto">
             
             <form action="" method="post">
     
-                <div class="mb-3">
-                  <input required type="text" class="form-control py-2" placeholder="E-mail Address" name="Email" value="Email"/>
+                <div className="mb-3">
+                  <input required type="text" className="form-control py-2" placeholder="E-mail Address" name="Email" value="Email"/>
                 </div>
     
-                <div class="mb-3">
-                  <input required type="text" class="form-control py-2" placeholder="Password" name="Password" value="Password"/>
+                <div className="mb-3">
+                  <input required type="text" className="form-control py-2" placeholder="Password" name="Password" value="Password"/>
                 </div>
               
-                <div class="row">
+                <div className="row">
                   <div>
-                    <button type="submit" class="btn btn-primary col-md-2">Login</button>
+                    <button type="submit" className="btn btn-primary col-md-2">Login</button>
                   </div>
                 </div>
             </form>
