257c0ba128652258f19a28e7d9ef46e09167b415
Manish Upadhyay <upadhyaymanish302@gmail.com>
2021-12-15 17:39:15 +0530

    search bar added

--- src/Components/Food.js
+++ src/Components/Food.js
@@ -4,17 +4,23 @@ import FoodDetails from './FoodDetails';
 import FooditemCard from './FooditemCard';
 import './Food.css';
 export default function Food(){
+    let query = '3lb carrots and a chicken sandwich and fries';
     const [data,setdata]=useState();
-    let query = '3lb carrots and a chicken sandwich and fries milk apple banana';
+    const [qry,setqry] = useState(query);
+    let vegies=['Carrot','Corn','Potato','Sweet Potato']
+    let beverages=[];
+    let fruits=['Apple','Banana','Grapes','Orange','Pear','Pineapple (1 cup)','Raspberries (1 cup)','Strawberries (1 cup)','Watermelon (1 cup)'];
+    let milk=['Low fat (2%) milk,Skim milk (1 cup),Yogurt (fruit-flavored, low fat) (1 cup)'];
+    let Beans=['Black eye peas','Garbanzo beans (chick peas)','Pinto beans','Refried beans','White beans']
+    // let query = '3lb carrots and a chicken sandwich and fries' + vegies.join(" ") + fruits.join(" ")// + milk.join(" ") + Beans.join(" ");
+    console.log(query);
     useEffect(()=>{
-        const apiUrl=`/nutrition?query=${query}`;
+        const apiUrl=`/nutrition?query=${qry}`;
         axios({
             method: 'GET',
             url: apiUrl,
             mode: 'cors',
             headers: {
-                // 'Access-Control-Allow-Origin':'*',
-        // 'Access-Control-Allow-Origin':'http://localhost:3000',
         'X-Api-Key':'Xrb1NEiAjSZc9yvEQykBNg==8KADYFa4P1NhV92q'},
         },function(error, response, body) {
             // console.log(response.json());
@@ -29,10 +35,43 @@ export default function Food(){
         });
 
     },[]);
+    const dataHandler = ()=>{
+        const apiUrl=`/nutrition?query=${qry}`;
+        axios({
+            method: 'GET',
+            url: apiUrl,
+            mode: 'cors',
+            headers: {
+        'X-Api-Key':'Xrb1NEiAjSZc9yvEQykBNg==8KADYFa4P1NhV92q'},
+        },function(error, response, body) {
+            // console.log(response.json());
+            if(error) return console.error('Request failed:', error);
+            else if(response.statusCode != 200) 
+            return console.error('Error:', response.statusCode, body.toString('utf8'));
+            else console.log(body)
+        }).then((response) => {
+            console.log(response.data)
+            setdata(response.data["items"]);
+           
+        });
+    }
     console.log(data);
     let loader=<div className="loader">Loading...</div>
     return data ? (
-        <div className="cards">
+        <div className="section">
+            <div className="app col-12 mb-3">
+                <input type="text" 
+                className="form-control" id="inputEmail4"
+                onChange={event=> setqry(event.target.value)}
+                placeholder="What did you eat Today?"/>
+
+             </div>
+             <div className="app">
+             <button className="btn btn-primary col-md"
+               onClick={()=>dataHandler()}>Search</button>
+             </div>
+             
+            <div className="cards">
             {
                 data && data.map((element,index)=>(
                     
@@ -46,31 +85,11 @@ export default function Food(){
                         sugar={element['sugar_g']}
                         fat={element['fat_total_g']}
                         />
-                        {/* <FoodDetails name={element['name']}
-                         cal={element['calories']}
-                         carbs={element['carbohydrates_total_g']}
-                         colestrol={element['cholesterol_mg']}
-                         protein={element['protein_g']}
-                         sugar={element['sugar_g']}
-                         fat={element['fat_total_g']}
-                         /> */}
                         
-                        {/* <h1>{element['name']}</h1>
-                        <h2>calories: </h2>
-                        <p>{element['calories']}</p>
-                        <h2>carbohydrates: </h2>
-                        <p>{element['carbohydrates_total_g']}g</p>
-                        <h2>cholesterol: </h2>
-                        <p>{element['cholesterol_mg']}mg</p>
-                        <h2>protein: </h2>
-                        <p>{element['protein_g']}g</p>
-                        <h2>sugar: </h2>
-                        <p>{element['sugar_g']}g</p>
-                        <h2>fat: </h2>
-                        <p>{element['fat_total_g']}g</p> */}
                     </div>
                 ))
             }
+        </div>
         </div>
     ): loader;
 
