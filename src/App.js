ba491f8cc4b48148ae6ac5b1956a42dac4b786d5
Manish Upadhyay <upadhyaymanish302@gmail.com>
2021-12-18 22:12:28 +0530

    food api

--- src/App.js
+++ src/App.js
@@ -81,7 +81,7 @@ function App() {
 
         <Route exact path = "/food">
         <div className="login">
-         <FoodDetails/>
+         {/* <FoodDetails/> */}
           <Food/>
         </div>
           
