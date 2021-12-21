708cc4b5c1e4efd3f0fe959576873d03a899d8ef
Akriti <akritisingh1811@gmail.com>
2021-12-21 17:31:56 +0530

    daily record updated

--- src/Components/DailyIntake.js
+++ src/Components/DailyIntake.js
@@ -4,6 +4,17 @@ export default function DailyIntake() {
     return (
         <div>
             <h2 className="py-4 text-center">DAILY CALORIES RECORD</h2>
+            <div className="card m-5">
+                <div className="card-body">
+                    <h4 className="card-title pb-2">25 DECEMBER, 2021</h4>
+                    
+                    <h5 className="py-4">Breakfast</h5>                  
+                    <h5 className="py-4">Lunch</h5>
+                    <h5 className="py-4">Snacks</h5>
+                    <h5 className="py-4">Dinner</h5>
+                    
+                </div>
+            </div>               
         </div>
     )
 }
